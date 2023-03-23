a13b755eb36ede1bd698ef71acf1fdf4ed275b1b
Erik-Z <erikzhou7@gmail.com>
2023-03-23 17:38:36 -0400

    Upgraded sqlx

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse, Responder};
-use sqlx::types::chrono::Utc;
+use chrono::Utc;
 use sqlx::PgPool;
+use sqlx::types::Uuid as SqlxUuid; 
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -10,12 +11,14 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> impl Responder {
+    let uuid_v4 = Uuid::new_v4();
+    let sqlx_uuid = SqlxUuid::from_u128_le(uuid_v4.to_u128_le());
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
         "#,
-        Uuid::new_v4(),
+        sqlx_uuid,
         form.email,
         form.name,
         Utc::now()
