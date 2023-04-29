dd05b2a78e8eeab617f5b9504bac4a840cc0f420
Erik-Z <erikzhou7@gmail.com>
2023-04-28 21:59:43 -0400

    Deployment files

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use email_newsletter::telemetry::{get_subscriber, init_subscriber};
 use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
+// use sqlx::postgres::PgPoolOptions;
 
 // TODO: Host a Jaeger instance on AWS and use OpenTelemetry with this project
 // TODO: Use tracing-error for better tracing/rust error integration
@@ -15,11 +16,16 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_string = configuration.database.connection_string();
-    let connection_pool = PgPool::connect(&connection_string.expose_secret())
-        .await
+    let connection_pool = PgPool::connect_lazy(&connection_string.expose_secret())
         .expect("Failed to connect to Postgres.");
+    // let connection_pool = PgPoolOptions::new()
+    //     .connect_timeout(std::time::Duration::from_secs(2))
+    //     .connect_lazy(&configuration.database.connection_string());
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
     run(listener, connection_pool)?.await
 }
